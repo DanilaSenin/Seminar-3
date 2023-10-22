09bde30b62d997d7a4dad199ef0a239cee429874
Danila Senin <16502711@mail.ru>
2023-10-22 23:57:44 +0300

    1 номер

---
+++ Domachka 1/Program.cs
@@ -0,0 +1,13 @@
+﻿int Step(int a, int b) {
+    int i = 0;
+int resul = 1;
+while (i < (b)) {
+    resul = resul*a;
+    i = i+1;
+}
+return resul;
+}
+Console.WriteLine("Введи свои числа, самурай: ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+Console.Write("Ваше число:"+ Step(a,b));
