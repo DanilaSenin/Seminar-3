14a2874f44bc2661886ed06064e2ba34886b74f3
Danila Senin <16502711@mail.ru>
2023-10-23 00:05:44 +0300

    Номер 2

---
+++ Domachka 2/Program.cs
@@ -0,0 +1,16 @@
+﻿void Standart () {
+   Console.Write("Введите число: "); 
+   
+}
+int SumCifr(int a) {
+int x = a;
+int res = 0;
+while (x > 0) {
+    res += x%10;
+    x = x/10;
+}
+return res;
+}
+Standart();
+int a = int.Parse(Console.ReadLine());
+Console.Write(SumCifr(a));
