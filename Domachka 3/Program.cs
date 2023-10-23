2b58a0926719d5cc804e6a48d9a5ed3f0ec0f3cf
Danila Senin <16502711@mail.ru>
2023-10-23 19:45:07 +0300

    Конец

---
+++ Domachka 3/Program.cs
@@ -0,0 +1,11 @@
+﻿void Arr() {
+int[] GG = new int [8];
+for (int i=0; i<8; i++) {
+    GG[i] = new Random().Next(1, 999);
+}
+for (int j = 0; j<8; j++) { 
+Console.WriteLine("Число №" + j +" в массиве = "+ GG[j]);
+}
+}
+
+Arr();
